1ac3660f95cdc7af6928c49d913d7fd7730305fa
Toma <13.35.104@gmail.com>
2020-10-17 17:19:16 +0900

    fix

--- rekognition.py
+++ rekognition.py
@@ -11,7 +11,7 @@ def encode_faces(filename):
     return bImgBase64
 
 
-def detect_faces(images_b64):
+def detect_faces(image_b64):
     response = client.detect_faces(
         Image={
             'Bytes': image_b64,
@@ -25,7 +25,7 @@ def detect_faces(images_b64):
 
 if __name__ == '__main__':
     args = sys.argv
-    filename = args[0]
+    filename = args[1]
     print(filename)
     result = detect_faces(encode_faces(filename))
     print(result)
