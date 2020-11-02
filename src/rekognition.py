d44bdcf1af2155d9676bea93359e80d7e2d1fce6
Toma <13.35.104@gmail.com>
2020-11-02 22:12:30 +0900

    refactoring

--- src/rekognition.py
+++ src/rekognition.py
@@ -1,4 +1,3 @@
-# import boto3
 import sys
 import json
 import asyncio
@@ -13,9 +12,6 @@ def get_image_from_file(filename):
         image_bytes= f.read()
 
     return image_bytes
-
-async def gather(args, detectfaces):
-    await asyncio.gather(*[detectfaces.detect_faces(x) for x in args])
 
 class DetectFaces:
     def __init__(self):
@@ -39,27 +35,20 @@ class DetectFaces:
     def get_result(self):
         return self.result
 
-async def main(args, detectfaces):
+async def main(args):
+    detectfaces = DetectFaces()
+
     async with aioboto3.client('rekognition',
                       region_name=AWS_PROFILES['AWS_DEFAULT_REGION'],
                       aws_access_key_id=AWS_PROFILES['AWS_ACCESS_KEY_ID'],
                       aws_secret_access_key=AWS_PROFILES['AWS_SECRET_ACCESS_KEY'],
     ) as client:
+        start = time.time()
         await asyncio.gather(*[detectfaces.detect_faces(x, client) for x in args])
+        elapsed = time.time() - start
+
+    print(f"{elapsed * 1000:.0f}ms")  # ミリ秒
+    print(detectfaces.get_result())
 
 if __name__ == '__main__':
-    args = sys.argv
-
-    args.pop(0)
-
-    detectfaces = DetectFaces()
-
-    start = time.time()
-    asyncio.run(main(args,detectfaces))
-    elapsed = time.time() - start
-
-    print(f"{elapsed:.3f}s")  # 秒
-    print(f"{elapsed * 1000:.0f}ms")  # ミリ秒
-    print(f"{elapsed * 1000000:.0f}μs")  # ナノ秒
-
-    print(detectfaces.get_result())
+    asyncio.run(main(sys.argv[1:]))
