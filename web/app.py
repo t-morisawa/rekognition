0c829ad8d1fd9aee29f78803c101fff67c0638f9
hikarukoizumi <hikaru.k.ac@gmail.com>
2020-11-08 13:36:30 +0900

    アクセスの許可

--- web/app.py
+++ web/app.py
@@ -1,6 +1,10 @@
 import responder
 
-api = responder.API()
+api = responder.API(cors=True, cors_params={
+    'allow_origins': ['*'],
+    'allow_methods': ['*'],
+    'allow_headers': ['*'],
+})
 
 @api.route("/")
 def hello_world(req, resp):
