368f9995995f8a36186e35610bdd39b957eaeed9
hikarukoizumi <hikaru.k.ac@gmail.com>
2020-11-14 18:01:26 +0900

    webの画像からバイナリへ変換

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -10,10 +10,21 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-key_account = input('Enter account name:')
-count_no = int(input('Set search count:'))
-search_results = tweepy.Cursor(api.user_timeline, screen_name=key_account).items(count_no)
+class GetTweetImage:
 
-for result in search_results:
-    img_url = result.extended_entities['media'][0]['media_url']
-    print(str(img_url))
+    def __init__(self):
+        self.result = []
+
+    def get_imge_url(self, accountName):
+        #key_account = input('Enter account name:')
+        #count_no = int(input('Set search count:'))
+        #search_results = tweepy.Cursor(api.user_timeline, screen_name=key_account).items(count_no)
+        search_results = tweepy.Cursor(api.user_timeline, screen_name=accountName).items(5)
+
+        for result in search_results:
+            img_url = result.extended_entities['media'][0]['media_url']
+            print(str(img_url))
+
+        self.result.append(img_url)
+
+    return self.result
