89cc59aecc407b29c3b42d3e26389b8ff9916971
Toma <13.35.104@gmail.com>
2020-11-21 16:09:34 +0900

    add Twitter.Vue

---
+++ front/src/components/Twitter.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <form>
+            <el-input placeholder="TwitterJP" v-model="input"></el-input>
+            <el-button @click="submit" type="submit">submit</el-button>
+        </form>
+        <div class="image-container">
+            <div v-for="datum in data" v-bind:key="datum.filename">
+               <el-image 
+                :src="datum.filename"
+                style="width: 200px; height: 200px"
+                fit="contain"></el-image>
+                <p v-if="datum.result">{{ datum.result[0].AgeRange.Low }} - {{ datum.result[0].AgeRange.High }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data: function () {
+        return {
+            input: '',
+            data: [],
+        }
+    },
+    methods: {
+        submit: function () {
+            const vm = this;
+            this.$axios
+                .get('http://localhost:8080/twitter' + this.data.input)
+                .then(function (response) {
+                    response.data.forEach(datum => {
+                        vm.data.push({
+                            "filename": datum.img_url,
+                            "result": datum.result.FaceDetails,
+                        });
+                    });
+                    console.log(vm.data)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    }
+}
+</script>
+<style scoped>
+.image-container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+}
+
+.upload {
+    margin-right: 20px;
+}
+
+.upload:hover {
+    color: #409EFF;
+}
+</style>
