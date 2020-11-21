fbd2451d4e155d387ce158ead1d200db0627c822
Toma <13.35.104@gmail.com>
2020-11-21 16:38:47 +0900

    update

--- front/src/components/Twitter.vue
+++ front/src/components/Twitter.vue
@@ -5,12 +5,14 @@
             <el-button @click="submit" type="submit">submit</el-button>
         </form>
         <div class="image-container">
-            <div v-for="datum in data" v-bind:key="datum.filename">
+            <div v-for="datum in data" v-bind:key="datum.img_url">
                <el-image 
-                :src="datum.filename"
+                :src="datum.img_url"
                 style="width: 200px; height: 200px"
                 fit="contain"></el-image>
-                <p v-if="datum.result">{{ datum.result[0].AgeRange.Low }} - {{ datum.result[0].AgeRange.High }}</p>
+                <p v-if="datum.result && datum.result[0]">
+                    {{ datum.result[0].AgeRange.Low }} - {{ datum.result[0].AgeRange.High }}
+                </p>
             </div>
         </div>
     </div>
@@ -29,14 +31,22 @@ export default {
         submit: function () {
             const vm = this;
             this.$axios
-                .get('http://localhost:8080/twitter' + this.data.input)
+                .get('http://localhost:8080/twitter/' + this.input)
                 .then(function (response) {
-                    response.data.forEach(datum => {
+                    // response.data.forEach(datum => {
+                    //     vm.data.push({
+                    //         "filename": datum.img_url,
+                    //         "result": datum.result.FaceDetails,
+                    //     });
+                    // });
+                    const res = response.data
+                    Object.keys(res).forEach(function(key) {
+                        var datum = this[key];
                         vm.data.push({
-                            "filename": datum.img_url,
+                            "img_url": datum.filename,
                             "result": datum.result.FaceDetails,
                         });
-                    });
+                    }, res);
                     console.log(vm.data)
                 })
                 .catch(function (error) {
